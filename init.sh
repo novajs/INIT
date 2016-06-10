49ced0aeb2f97b14bed5bbca6f5d188bcc0e7c52
Jared Allard <jaredallard@outlook.com>
2016-06-09 18:48:51 -0700

    init: better verbosity

--- init.sh
+++ init.sh
@@ -3,9 +3,11 @@
 echo "Grabbing Assignment Files"
 echo "ID: $ASSIGNMENTID"
 
-pushd "/workspace"
-git clone --depth=1 "$(node /root/.novajs/assignment.js)"
-popd
+cd /workspace
+export URL="$(node /root/.novajs/assignment.js)"
+echo "URL: $URL"
+git clone --depth=1 "$URL"
+cd /root
 
 echo "Spawning supervisord to watch over cloud9 process."
 supervisord -c "/etc/supervisor/supervisord.conf"
