df479442fca09ee95fafc1cdeb5675750f56de9a
Jared Allard <jaredallard@users.noreply.github.com>
2016-09-12 22:03:08 -0700

    init: don't daemonize pm2

--- init.sh
+++ init.sh
@@ -29,5 +29,4 @@ fi
 cd /cloud9
 
 echo "I: Spawing PM2"
-pm2 start --name cloud9 server.js -- -p 80 -a -w "/workspace" &
-echo "I: Daemonized."
+pm2 start --no-daemon --name cloud9 server.js -- -p 80 -a -w "/workspace"
